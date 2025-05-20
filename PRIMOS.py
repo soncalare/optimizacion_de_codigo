a93d59624ca9bff3fb7dd208add246dfad591bff
soncalare <soncalare10@gmail.com>
2025-05-20 00:20:33 -0500

    empieza optimizacion

---
+++ PRIMOS.py
@@ -0,0 +1,19 @@
+import time
+
+start = time.time()
+
+def es_primo(n):
+    if n < 2:
+        return False
+    for i in range(2, int(n**0.5)+1):
+        if n % i == 0:
+            return False
+    return True
+
+primos = [n for n in range(1, 100001) if es_primo(n)]
+
+# Imprimir la cantidad total
+print(f"Cantidad total de primos encontrados: {len(primos)}")
+
+end = time.time()
+print(f"Tiempo de ejecución: {end - start:.2f} segundos")
